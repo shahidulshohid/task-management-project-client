b10fcd85b998fc2f954654441ca401b6220b418f
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-21 02:36:10 +0600

    my task page is done

--- src/pages/MyTask/MyTask.jsx
+++ src/pages/MyTask/MyTask.jsx
@@ -34,13 +34,88 @@ const MyTask = () => {
         <HiOutlinePlusSm size={20} />
         Add task
       </button>
-
-
+      
       <div className="p-4">
       {/* To-Do Section */}
       <div>
         <h2 className="text-xl font-bold cursor-pointer" onClick={handleToDoClick}>
           To-Do
+        </h2>
+
+        {/* Add Task Button */}
+        {showAddButton && !showInputField && (
+          <button 
+            onClick={handleAddTaskClick} 
+            className="flex items-center mt-2 px-2 py-1 bg-[#4A00FF] text-white rounded"
+          >
+            <HiOutlinePlusSm size={20} /> Add Task
+          </button>
+        )}
+
+        {/* Task Input Field */}
+        {showInputField && (
+          <div className="mt-2">
+            <input
+              type="text"
+              placeholder="Write task title..."
+              value={taskTitle}
+              onChange={(e) => setTaskTitle(e.target.value)}
+              className="border p-2 rounded w-full"
+            />
+            <button 
+              onClick={handleTaskSubmit} 
+              className="flex items-center mt-2 px-2 py-1 bg-[#4A00FF] text-white rounded"
+            >
+             <HiOutlinePlusSm size={20} /> Add Task
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+
+    <div className="p-4">
+      {/* progress Section */}
+      <div>
+        <h2 className="text-xl font-bold cursor-pointer" onClick={handleToDoClick}>
+          In Progress
+        </h2>
+
+        {/* Add Task Button */}
+        {showAddButton && !showInputField && (
+          <button 
+            onClick={handleAddTaskClick} 
+            className="flex items-center mt-2 px-2 py-1 bg-[#4A00FF] text-white rounded"
+          >
+            <HiOutlinePlusSm size={20} /> Add Task
+          </button>
+        )}
+
+        {/* Task Input Field */}
+        {showInputField && (
+          <div className="mt-2">
+            <input
+              type="text"
+              placeholder="Write task title..."
+              value={taskTitle}
+              onChange={(e) => setTaskTitle(e.target.value)}
+              className="border p-2 rounded w-full"
+            />
+            <button 
+              onClick={handleTaskSubmit} 
+              className="flex items-center mt-2 px-2 py-1 bg-[#4A00FF] text-white rounded"
+            >
+             <HiOutlinePlusSm size={20} /> Add Task
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+
+    <div className="p-4">
+      {/* Done Section */}
+      <div>
+        <h2 className="text-xl font-bold cursor-pointer" onClick={handleToDoClick}>
+          Done
         </h2>
 
         {/* Add Task Button */}
